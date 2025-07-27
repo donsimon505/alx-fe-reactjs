ddb17b473ce0f22dab5e0161e962d3f03b0e141d
Stephen Doe <donsimon505@gmail.com>
2025-07-27 22:02:36 +0000

    Added Styling to Components

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,7 +1,9 @@
 function Header() {
   return (
-    <header>
-      <h1>My Favorite Cities</h1>
+    <header
+      style={{ backgroundColor: "navy", color: "white", textAlign: "center" }}
+    >
+      <h1 style={{ padding: "10px" }}>My Favorite Cities</h1>
     </header>
   );
 }
